9e4f9b0d666212e01fc750b7482d20f1f40b787b
Chiefpro <chiefpro@yandex.ru>
2023-07-30 20:57:45 +0300

    s2.2

--- S2.java
+++ S2.java
@@ -10,7 +10,19 @@ public class S2 {
         System.out.println(newStr);
 
         System.out.println("--------------------  Задача 2 -----------------");
+        String str1 = "aaaabbbcd";
+        System.out.println(str1);
+        String str2 = shatie(str1);
+        System.out.println(str2);
+
+        System.out.println("--------------------  Задача 3 -----------------");
+        String str3 = "abccfba";
+        boolean flag = palindrom(str3);
+        System.out.println(flag);
+    
+    
     }
+
 
     // public static String metod(int n, char c1, char c2) {
     // StringBuilder sb = new StringBuilder();
@@ -38,4 +50,60 @@ public class S2 {
         }
         return sb.toString();
     }
+
+// Напишите метод, который сжимает строку. Пример: вход aaaabbbcddaa.
+// a4b3cd2a2
+
+
+        public static String shatie(String str) {
+            StringBuilder sb = new StringBuilder();
+            Character temp = null;
+            Character last = null;
+            int countTemp = 1;
+            // aabbcdaa
+            for (int i = 1; i < str.length(); i++) {
+                temp = str.charAt(i); // d
+                last = str.charAt(i - 1); //c 
+                if (temp == last) {
+                    countTemp++;
+                } else {
+                    if (countTemp == 1) {
+                        sb.append(last);
+                    } else {
+                        sb.append(last).append(countTemp);
+                        countTemp = 1;
+                    }
+                }
+            }
+            if (countTemp == 1) {
+                sb.append(temp);
+            } else {
+                sb.append(temp).append(countTemp);
+            }
+            return sb.toString();
+        }
+
+
+
+// Напишите метод, который принимает на вход строку (String) и определяет 
+// является ли строка палиндромом (возвращает boolean значение).
+// abcba
+
+        public static Boolean palindrom(String str) {
+        for (int i = 0; i < str.length() / 2; i++) {
+            // a    b   a
+            // 0    1   2
+            // 
+            // str.charAt(i) = 0
+            // str.charAt(str.length() - i - 1) = 2
+            //                  3      - 0 - 1  = 2
+            if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    
 }
