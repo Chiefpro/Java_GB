c70d7a28c409b32da07f77e83c3dde850f12e25c
Chiefpro <chiefpro@yandex.ru>
2023-07-30 16:05:57 +0300

    an2.0

--- SortUtils.java
+++ SortUtils.java
@@ -34,7 +34,75 @@ public class SortUtils {
     }
 
     private static void quikSort(int[] arr, int start, int end) {
+        int left = start, right = end;
+        int middle = arr[(start + end) / 2]; // хначение опорного эллемента
 
+        do {
+            while (arr[left] < middle) {
+                left++;
+            }
+            while (arr[right] > middle) {
+                right--;
+            }
+            if (left <= right) {
+                if (left < right) {
+                    int buf = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = buf;
+                }
+                left++;
+                right--;
+            }
+
+        } while (left <= right);
+
+        if (left < end) {
+            quikSort(arr, left, end);
+        }
+        if (start < right) {
+            quikSort(arr, start, right);
+        }
+    }
+
+    /**
+     * Пирамидальная сортировка
+     * 
+     * @param arr
+     * @return
+     */
+    public static int[] heapSoft(int[] arr) {
+        for (int i = arr.length / 2 - 1; i >= 0; i--)
+            heapify(arr, arr.length, i);
+
+        for (int i = arr.length - 1; i >= 0; i++) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+        return arr;
+
+    }
+
+    private static void heapify(int[] arr, int heapSize, int rootIndex) {
+        int largest = rootIndex;
+        int leftChild = 2 * rootIndex + 1;
+        int rightChild = 2 * rootIndex + 2;
+
+        if (leftChild < heapSize && arr[leftChild] > arr[largest])
+            largest = leftChild;
+
+        if (rightChild < heapSize && arr[rightChild] > arr[largest])
+            largest = rightChild;
+
+        if (largest != rootIndex) {
+            int temp = arr[rootIndex];
+            arr[rootIndex] = arr[largest];
+            arr[largest] = temp;
+
+            heapify(arr, heapSize, largest);
+        }
     }
 
 }
