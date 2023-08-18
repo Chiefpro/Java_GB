5915309154e9f7e2e7ed3be0d2446667a95003c5
Chiefpro <chiefpro@yandex.ru>
2023-08-19 00:44:16 +0300

    JavaCore 01.10

--- JavaCore/HW_01.java
+++ JavaCore/HW_01.java
@@ -2,8 +2,43 @@ package JavaCore;
 
 public class HW_01 {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("hrt");
+        printThreeWords();
+        System.out.println();
+        checkSumSign(2, -12);
+        System.out.println();
+        printColor(1);
+        System.out.println();
+        compareNumbers(4, 2);
     }
-    
+
+    public static void printThreeWords() {
+        System.out.println("baban");
+        System.out.println("orange");
+        System.out.println("apple");
+    }
+
+    public static void checkSumSign(int a, int b) {
+        if (a + b >= 0)
+            System.out.println("Polozhitelno");
+        else
+            System.out.println("Otricatelno");
+    }
+
+    public static void printColor(int value) {
+        if (value <= 0)
+            System.out.println("Red");
+        else if (value > 0 && value < 100)
+            System.out.println("Elow");
+        else
+            System.out.println("Green");
+
+    }
+
+    public static void compareNumbers(int a, int b){
+        if (a>=b)
+            System.out.println("a >= b");
+        else
+            System.out.println("a < b");
+    }
+
 }
