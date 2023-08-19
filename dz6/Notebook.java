2fe5094618edb75e2826f0a5415030e340dda5ec
Chiefpro <chiefpro@yandex.ru>
2023-08-19 22:30:05 +0300

    dz6.3

--- dz6/Notebook.java
+++ dz6/Notebook.java
@@ -1,23 +1,6 @@
-// package 6_Java.Java_GB.dz6.1;/
-
 import java.util.*;
 
-/*
-Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
-Создать множество ноутбуков.
-Написать метод, который будет запрашивать у пользователя критерий (или критерии) фильтрации и выведет ноутбуки,
-отвечающие фильтру. Критерии фильтрации можно хранить в Map.
-Например: “Введите цифру, соответствующую необходимому критерию:
-1 - ОЗУ
-2 - Объем ЖД
-3 - Операционная система
-4 - Цвет …
-Далее нужно запросить минимальные значения для указанных критериев - сохранить параметры фильтрации можно также в Map.
-Отфильтровать ноутбуки из первоначального множества и вывести проходящие по условиям.
-
- */
 public class Notebook {
-    // ОПРЕДЕЛЯЕМ ПОЛЯ КЛАССА NOTEBOOK
     String name;
     double diagonal;
     int maxRAM;
@@ -26,7 +9,6 @@ public class Notebook {
     String color;
     int price;
 
-    // КОНСТРУКТОР КЛАССА NOTEBOOK
     public Notebook(String name, double diagonal, int maxRAM, int maxHDD, String opSystem, String color, int price) {
         this.name = name;
         this.diagonal = diagonal;
@@ -37,7 +19,6 @@ public class Notebook {
         this.price = price;
     }
 
-    // ОПРЕДЕЛЯЕМ ПОРЯДОК ВЫВОДА ИНФОРМАЦИИ НА ПЕЧАТЬ
     @Override
     public String toString() {
         return "\nНоутбук:             " + name +
@@ -49,64 +30,56 @@ public class Notebook {
                 "\nЦена:                " + price;
     }
 
-
     public static void menu() {
-        System.out.print("""Уважаемый пользователь, 
-                для быстрого поиска ноутбука, укажите
-                требования к его характеристикам:
-                выбор диаганали        - нажмите 1
-                выбор Макс. объем RAM  - нажмите 2
-                выбор жесткого диска   - нажмите 3
-                выбора типа ОС         - нажмите 4
-                выбор цвета            - нажмите 5
-                задать все пареметры   - нажмите 6
-                Ваше решение: \s""";);
+        System.out.print("""
+                Уважаемый пользователь, для быстрого поиска ноутбука, укажите требования к его характеристикам: 
+                выбор диаганали        -  1
+                выбор Макс. объем RAM  -  2
+                выбор жесткого диска   -  3
+                выбора типа ОС         -  4
+                выбор цвета            -  5
+                задать все пареметры   -  6
+                Ваше решение: \s""");
     }
 
-    // ФОРМИРУЕМ МНОГОВАРИАНТНЫЕ ЗАПРОСЫ ПОЛЬЗОВАТЕЛЯ ДЛЯ ДОБАВЛЕНИЯ В ФИЛЬТР
     public static Set<Object> normDemands(Set<Notebook> mySet) {
         Scanner aScanner = new Scanner(System.in);
         int decisionNumber = aScanner.nextInt();
-        String text1 = "
-                В настоящее время доступны ноутбуки,
-                имеющие следующие диаганали:
+        String text1 = """
+                В настоящее время доступны ноутбуки, имеющие следующие диаганали: 
                 - 14.5 дюймов;
                 - 15.6 дюймов;
                 - 17.3 дюйма.
-                Введите нужное значение: \s";
+                Введите нужное значение: \s""";
 
-        String text2 = "
-                В настоящее время доступны ноутбуки,
-                имеющие максимальный объем ОЗУ:
+        String text2 = """
+                В настоящее время доступны ноутбуки, имеющие максимальный объем ОЗУ: 
                 - 4  ГБ;
                 - 8  ГБ;
                 - 16 ГБ.
-                Введите нужное значение: \s";
+                Введите нужное значение: \s""";
 
-        String text3 = "
-                В настоящее время доступны ноутбуки,
-                имеющие следующие объемы ЖД:
+        String text3 = """
+                В настоящее время доступны ноутбуки, имеющие следующие объемы ЖД:
                 - 1000 ГБ;
                 - 512  ГБ;
                 - 500  ГБ;
                 - 256  ГБ.
-                Введите нужное значение: \s";
+                Введите нужное значение: \s""";
 
-        String text4 = "
-                В настоящее время доступны ноутбуки,
-                имеющие следующие операционные системы:
+        String text4 = """
+                В настоящее время доступны ноутбуки, имеющие следующие операционные системы:
                 - Windows10;
                 - Windows11;
                 - MacOS.
-                Введите нужную ОС: \s";
+                Введите нужную ОС: \s""";
 
-        String text5 = "
-                В настоящее время доступны ноутбуки,
-                следующих цветов:
+        String text5 = """
+                В настоящее время доступны ноутбуки, следующих цветов:
                 - silver;
                 - gray;
                 - black.
-                Введите нужный цвет: \s";
+                Введите нужный цвет: \s""";
 
         double diagonal;
         int maxRAM;
@@ -115,49 +88,54 @@ public class Notebook {
         String color;
 
         Set<Object> request = new HashSet<>();
-        if (decisionNumber == 1) {                              // Блок фильтра по диаганали ноутбука.
+        if (decisionNumber == 1) { // Блок фильтра по диаганали ноутбука.
             System.out.printf("\n%s", text1);
             Scanner bScanner = new Scanner(System.in);
             diagonal = Double.parseDouble(bScanner.next());
             request.add(diagonal);
             result(mySet, request);
+            bScanner.close();
             return request;
 
         }
-        if (decisionNumber == 2) {                              // Блок фильтра по ОЗУ.
+        if (decisionNumber == 2) { // Блок фильтра по ОЗУ.
             System.out.printf("\n%s", text2);
             Scanner cScanner = new Scanner(System.in);
             maxRAM = cScanner.nextInt();
             request.add(maxRAM);
             result(mySet, request);
+            cScanner.close();
             return request;
 
         }
-        if (decisionNumber == 3) {                              // Блок фильтра по жесткому диску.
+        if (decisionNumber == 3) { // Блок фильтра по жесткому диску.
             System.out.printf("\n%s", text3);
             Scanner dScanner = new Scanner(System.in);
             maxHDD = dScanner.nextInt();
             request.add(maxHDD);
             result(mySet, request);
+            dScanner.close();
             return request;
         }
-        if (decisionNumber == 4) {                              // Блок фильтра по операционной системе.
+        if (decisionNumber == 4) { // Блок фильтра по операционной системе.
             System.out.printf("\n%s", text4);
             Scanner eScanner = new Scanner(System.in);
             opSystem = eScanner.next();
             request.add(opSystem);
             result(mySet, request);
+            eScanner.close();
             return request;
         }
-        if (decisionNumber == 5) {                               // Блок фильтра по цвету ноутбука.
+        if (decisionNumber == 5) { // Блок фильтра по цвету ноутбука.
             System.out.printf("\n%s", text5);
             Scanner fScanner = new Scanner(System.in);
             color = fScanner.next();
             request.add(color);
             result(mySet, request);
+            fScanner.close();
             return request;
         }
-        if (decisionNumber == 6) {                               // Блок фильтра по всем параметрам.
+        if (decisionNumber == 6) { // Блок фильтра по всем параметрам.
             System.out.printf("\n%s", text1);
             Scanner bScanner = new Scanner(System.in);
             diagonal = Double.parseDouble(bScanner.next());
@@ -183,9 +161,15 @@ public class Notebook {
             color = fScanner.next();
             request.add(color);
             equals(mySet, request);
+            bScanner.close();
+            cScanner.close();
+            dScanner.close();
+            eScanner.close();
+            fScanner.close();
             return request;
-
+            
         }
+        aScanner.close();
         return request;
     }
 
@@ -209,7 +193,7 @@ public class Notebook {
         }
         if (!flag) {
             System.out.println("\nК сожалению товаров с указанными характеристиками сейчас нет. " +
-                    "Проконсультироваться о возможности заказа ноутбуков можно у менеджера (тел. +7-999-888-77-66)");
+                    "Проконсультироваться о возможности заказа ноутбуков можно у менеджера");
         }
     }
 
@@ -220,7 +204,8 @@ public class Notebook {
         for (Notebook notebook : mySet) {
             for (Object ignored : request) {
                 if (request.contains(notebook.diagonal) && request.contains(notebook.maxRAM)
-                        && request.contains(notebook.maxHDD) && request.contains(notebook.opSystem) && request.contains(notebook.color)) {
+                        && request.contains(notebook.maxHDD) && request.contains(notebook.opSystem)
+                        && request.contains(notebook.color)) {
                     flag = true;
                     System.out.println(notebook);
                     break;
@@ -230,7 +215,7 @@ public class Notebook {
         }
         if (!flag) {
             System.out.println("\nК сожалению товаров с указанными характеристиками сейчас нет. " +
-                    "Проконсультироваться о возможности заказа ноутбуков можно у менеджера (тел. +7-999-888-77-66)");
+                    "Проконсультироваться о возможности заказа ноутбуков можно у менеджера");
         }
     }
 }
