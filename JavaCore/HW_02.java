e6c077237553df2843c98d2b536fa36b55032b8b
Chiefpro <chiefpro@yandex.ru>
2023-08-20 22:38:34 +0300

    JavaCore 02.01

--- JavaCore/HW_02.java
+++ JavaCore/HW_02.java
@@ -3,5 +3,7 @@ package JavaCore;
 public class HW_02 {
     public static void main(String[] args) {
         System.out.println("Hello");
+        String s = "HEllo" + 10 + " fefef";
+        System.out.println(s);
     }
 }
