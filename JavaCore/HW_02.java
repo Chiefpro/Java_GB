e5e0f4e02404bb8e0041c0b4992d34dbff20370a
Chiefpro <chiefpro@yandex.ru>
2023-08-21 00:11:32 +0300

    JavaCore 02.10

--- JavaCore/HW_02.java
+++ JavaCore/HW_02.java
@@ -1,9 +1,38 @@
 package JavaCore;
 
+import java.util.Scanner;
+
 public class HW_02 {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        String s = "HEllo" + 10 + " fefef";
-        System.out.println(s);
+        System.out.println(w1(3, 6));
+        System.out.println(z2(0));
+        System.out.println(z3(-4));
+        z4("Hello", 5);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введи год => ");
+        System.out.println(z5(sc.nextInt()));
+        sc.close();
     }
+
+    public static boolean w1(int a, int b) {
+        return ((a + b) <= 20 && (a + b) > 10) ? true : false;
+    }
+
+    public static String z2(int a) {
+        return a >= 0 ? "Положительное" : "Отрицательное";
+    }
+
+    public static boolean z3(int a) {
+        return a >= 0 ? true : false;
+    }
+
+    public static void z4(String s, int a) {
+        for (int i = 0; i < a; i++)
+            System.out.println(s);
+    }
+
+    public static boolean z5(int a) {
+        return (a % 4 == 0 && a % 100 != 0) || a % 400 == 0  ? true : false;
+    }
+
 }
