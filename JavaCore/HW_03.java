bde4ad12ea271e44ee81ad0c5f0aa8801bbe27d8
Chiefpro <chiefpro@yandex.ru>
2023-08-23 22:51:42 +0300

    JavaCore 03.01

--- JavaCore/HW_03.java
+++ JavaCore/HW_03.java
@@ -1,11 +1,14 @@
 package JavaCore;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * HW_03
  */
 public class HW_03 {
 
     public static void main(String[] args) {
-        
+        System.out.println(Math.pow(2, 4));
     }
 }
