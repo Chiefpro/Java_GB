56cbea2576bcc42c2773c5820172932250bf7b66
Chiefpro <chiefpro@yandex.ru>
2023-08-24 22:13:30 +0300

    JavaCore 03.02

--- JavaCore/HW_03.java
+++ JavaCore/HW_03.java
@@ -10,5 +10,7 @@ public class HW_03 {
 
     public static void main(String[] args) {
         System.out.println(Math.pow(2, 4));
+        String s = "Vasja"; int n = 4;
+        System.out.printf("Привет %s %d раз!!!", s, n);
     }
 }
