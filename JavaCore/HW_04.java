3cbdcc09abb225208eb0fba92f35087ddf9bb9d4
Chiefpro <chiefpro@yandex.ru>
2023-08-27 22:03:18 +0300

    web_4.00

---
+++ JavaCore/HW_04.java
@@ -0,0 +1,12 @@
+package JavaCore;
+
+/**
+ * HW_04
+ */
+public class HW_04 {
+
+    public static void main(String[] args) {
+        System.out.println( -100.0 / 0.0 );
+        System.out.println(0.0 / 0.0);
+    }
+}
