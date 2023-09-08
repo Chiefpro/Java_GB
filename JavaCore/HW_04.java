628dbadf6baf31567689adf1e1d2deb9f3ff8149
Chiefpro <chiefpro@yandex.ru>
2023-09-09 00:08:29 +0300

    JavaCore 04.0014

--- JavaCore/HW_04.java
+++ JavaCore/HW_04.java
@@ -6,75 +6,12 @@ import java.util.Random;
  * HW_04
  */
 public class HW_04 {
-    private static final String CERTAIN = "Бесспорно";
-    private static final String DEFINITELY = "Определённо да";
-    private static final String MOST_LIKELY = "Вероятнее всего";
-    private static final String OUTLOOK_GOOD = "Хорошие перспективы";
-    private static final String ASK_AGAIN_LATER = "Спроси позже";
-    private static final String TRY_AGAIN = "Попробуй снова";
-    private static final String NO = "Мой ответ — нет";
-    private static final String VERY_DOUBTFUL = "Весьма сомнительно";
+    
 
     public static void main(String[] args) {
-        System.out.println(-100.0 / 0.0);
-        System.out.println(0.0 / 0.0);
-
-        // gergeffffg456
-        System.out.println(pow(2, 3));
-
-        System.out.println("Заработаю ли я себе на BMW?");
-        System.out.println(getPrediction());
-        System.out.println((int) (Math.random() * 6));
-        int x = (3 & 4);
-        System.out.println(x);
+        System.out.println("ggggggggggggggggggggg");
     }
 
-    public static double pow(int number, int power) {
-        if (power == 0) {
-            return 1;
-        }
-
-        int modulus = power < 0 ? power * -1 : power;
-        int result = (int) Math.pow(number, modulus);
-        return result;
-    }
-
-    public static String getPrediction() {
-        // напишите тут ваш код
-        String resalt = null;
-        Random rn = new Random();
-        int t = rn.nextInt(8);
-        System.out.println(t);
-        switch (t) {
-            case 0:
-                resalt = CERTAIN;
-                break;
-            case 1:
-                resalt = DEFINITELY;
-                break;
-            case 2:
-                resalt = MOST_LIKELY;
-                break;
-            case 3:
-                resalt = OUTLOOK_GOOD;
-                break;
-            case 4:
-                resalt = ASK_AGAIN_LATER;
-                break;
-            case 5:
-                resalt = TRY_AGAIN;
-                break;
-            case 6:
-                resalt = NO;
-                break;
-            case 7:
-                resalt = VERY_DOUBTFUL;
-                break;
-
-            default:
-                break;
-        }
-        return resalt;
-    }
+    
 
 }
