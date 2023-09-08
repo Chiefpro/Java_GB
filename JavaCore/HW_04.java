5be807e4b7a33a50c89883e8b014c38981aa7293
Chiefpro <chiefpro@yandex.ru>
2023-09-09 00:22:56 +0300

    JavaCore 04.10

--- JavaCore/HW_04.java
+++ JavaCore/HW_04.java
@@ -1,12 +1,30 @@
 package JavaCore;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * HW_04
  */
 public class HW_04 {
-    
+    private static final char DOT_X = 'X';
+    private static final char DOT_O = 'O';
+    private static final char DOT_EMPTY = '.';
+
+    private static final Scanner sc = new Scanner(System.in);
+    private static final Random rn = new Random();
+
+    private static char[][] field;
+    private static char dotHuman;
+    private static char dotAi;
+    private static int fieldSizeX;
+    private static int fieldSizeY;
+    private static int scoreHuman;
+    private static int scoreAi;
+    private static int RoundCounter;
+    private static int WinLength;
+
+
 
     public static void main(String[] args) {
         System.out.println("ggggggggggggggggggggg");
