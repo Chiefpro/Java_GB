9156314503617f5a4e8fe7e63a5fbd284b49d3e8
Chiefpro <chiefpro@yandex.ru>
2023-09-15 21:25:59 +0300

    JavaCore 05.0013

--- JavaCore/HW_05.java
+++ JavaCore/HW_05.java
@@ -39,14 +39,9 @@ public class HW_05 {
             octalNumber = octalNumber / 10;
         }
         return decimalNumber;
-        
-
-
-        
+               
         
     }
-
-    
-
+  
     
 }
