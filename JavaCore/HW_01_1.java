e2f99fbf55f1d9a6dbd66f331101760f4f02b0e2
Chiefpro <chiefpro@yandex.ru>
2024-01-09 17:16:25 +0300

    snova 002

---
+++ JavaCore/HW_01_1.java
@@ -0,0 +1,9 @@
+package JavaCore;
+
+public class HW_01_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!!");
+        System.out.println(5+7);
+    }
+    
+}
