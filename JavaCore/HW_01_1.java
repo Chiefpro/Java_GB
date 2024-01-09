c347cbbfb265344476b81304c567bf8af0c5b99d
Chiefpro <chiefpro@yandex.ru>
2024-01-09 20:24:04 +0300

    povtor 002.4

--- JavaCore/HW_01_1.java
+++ JavaCore/HW_01_1.java
@@ -2,10 +2,19 @@
 
 public class HW_01_1 {
     public static void main(String[] args) {
+        proba1();
+
+    }
+    
+    public static void proba1(){
+        
         System.out.println("Hello World!!!!");
         System.out.println(5+7);
         String str = "Proba pera";
         System.out.println(str);
-    }
+}
+
+
+
     
 }
