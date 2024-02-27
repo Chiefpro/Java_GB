f25ab4221fbaf36ba45036a1a4c5161503474885
Chiefpro <chiefpro@yandex.ru>
2024-02-27 20:26:24 +0300

    JR 28

--- JavaCore/JR.java
+++ JavaCore/JR.java
@@ -1,14 +1,28 @@
+import java.util.Scanner;
+
 public class JR {
     public static void main(String[] args) {
         zadacha24();
     }
 
-    public static void zadacha24(){
+    public static void zadacha24() {
         int number = 546;
 
-        int lastDigit; //напишите тут ваш код
+        int lastDigit; // напишите тут ваш код
         lastDigit = number % 10;
 
         System.out.println(lastDigit);
+
+        String bigAmount = "500";
+        String greatAmount = "100000";
+
+        int hugeAmount = Integer.parseInt(greatAmount) + Integer.parseInt(bigAmount);
+
+        System.out.println(hugeAmount);
+
+        System.out.print("Введи число => ");
+        Scanner sc = new Scanner(System.in);
+        int za45 = sc.nextInt();
+        System.out.println("Ты ввел => " + za45);
     }
 }
