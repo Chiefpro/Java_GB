1ccf163fa3b3c965b2665f4886c0d2f60d1d2190
chief <chiefpro@yandex.ru>
2024-02-28 20:32:54 +0300

    JR 3.21

--- JavaCore/JR.java
+++ JavaCore/JR.java
@@ -6,37 +6,35 @@ public class JR {
     }
 
     public static void zadacha24() {
-        int number = 546;
-
-        int lastDigit; // напишите тут ваш код
-        lastDigit = number % 10;
-
-        System.out.println(lastDigit);
-
-        String bigAmount = "500";
-        String greatAmount = "100000";
-
-        int hugeAmount = Integer.parseInt(greatAmount) + Integer.parseInt(bigAmount);
-
-        System.out.println(hugeAmount);
-
         Scanner sc = new Scanner(System.in);
-        // System.out.print("Введи первую строку (нижний) => ");
-        // String z1 = sc.nextLine().toLowerCase();
-        // System.out.print("Введи вторую строку (верхний) => ");
-        // String z2 = sc.nextLine().toUpperCase();
-        // System.out.print("Введи третью строку => ");
-        // String z3 = sc.nextLine();
-        // System.out.println(z1 + "\n" + z2 + "\n" + z3);
-
-        // int z4 = (sc.nextInt() + sc.nextInt() + sc.nextInt()) / 3;
-        int z5;
-        if (sc.hasNextInt()){
-            z5 = sc.nextInt();
-        System.out.println("катит " + z5);
+        int temp;
+        while (true) {
+            System.out.print("Введи температуру => ");
+            boolean tt = sc.hasNextInt();
+            if (!tt) {
+                System.out.println("Ты ввел не число!!!");
+            } else {
+                temp = sc.nextInt();
+                break;
+            }
         }
+        System.out.println(temp >= 0 ? "тепло" : "холодно");
+
+        int d1, d2, d3;
+        d1 = sc.nextInt();
+        d2 = sc.nextInt();
+        d3 = sc.nextInt();
+
+        if (d2 == d3 && d2 == d1)
+            System.out.println(d2 + " " + d3 + " " + d1);
+        else if (d2 == d3)
+            System.out.println(d2 + " " + d3);
+        else if (d1 == d3)
+            System.out.println(d1 + " " + d3);
+        else if (d1 == d2)
+            System.out.println(d1 + " " + d2);
         else
-            System.out.println("не катит");
-        // System.out.println(z4);
+            System.out.println("нет таких");
     }
+
 }
