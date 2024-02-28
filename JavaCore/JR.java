00b64231b21f830c67a7090f2c771a95373ca290
chief <chiefpro@yandex.ru>
2024-02-28 22:14:35 +0300

    JR 3.23

--- JavaCore/JR.java
+++ JavaCore/JR.java
@@ -7,30 +7,15 @@ public class JR {
 
     public static void zadacha24() {
         Scanner sc = new Scanner(System.in);
-        int temp;
-        while (true) {
+        int temp = 0;
+        do {
             System.out.print("Введи температуру => ");
-            boolean tt = sc.hasNextInt();
-            if (!tt) {
-                System.out.println("Ты ввел не число!!!");
-            } else {
-                temp = sc.nextInt();
-                break;
-            }
-        }
+            temp = sc.nextInt();
+            
+        }while (sc.hasNextInt());
         System.out.println(temp >= 0 ? "тепло" : "холодно");
 
-        String d1,d2;
         
-        d2 = "DD";
-        System.out.println("vvedi D1");
-        d1 = sc.next();
-        System.out.println("Vvedi D2");
-        d2 = sc.next();
-        System.out.println(d1);
-        System.out.println(d2);
-        
-        System.out.println(d1.equalsIgnoreCase(d2)?"Одинаковые":"разные");
 
     }
 
