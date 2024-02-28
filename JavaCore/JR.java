69f1d01f1d38d28072af7cf28091bf43b39d06bc
chief <chiefpro@yandex.ru>
2024-02-28 22:26:14 +0300

    JR 3.24

--- JavaCore/JR.java
+++ JavaCore/JR.java
@@ -7,13 +7,20 @@ public class JR {
 
     public static void zadacha24() {
         Scanner sc = new Scanner(System.in);
-        int temp = 0;
-        do {
+        int temp =0 ;
+        boolean br = true;
+        while (br) {
             System.out.print("Введи температуру => ");
-            temp = sc.nextInt();
+            if (sc.hasNextInt()==true){
+                temp = sc.nextInt();
+                br = false;
+            }
+        
             
-        }while (sc.hasNextInt());
-        System.out.println(temp >= 0 ? "тепло" : "холодно");
+            
+            
+        }
+        System.out.println(temp >=0 ? "тепло" : "холодно");
 
         
 
