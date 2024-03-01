5c6961fbe4419b2fc60df75124a768fb6a3d8408
chief <chiefpro@yandex.ru>
2024-03-02 00:10:52 +0300

    JR 4.4.4

--- JavaCore/JR.java
+++ JavaCore/JR.java
@@ -4,7 +4,9 @@ public class JR {
     public static void main(String[] args) {
         // zadacha24();
         // zadacha41(10);
-        zadacha42(10, 20);
+        // zadacha42(10, 20);
+        // zadacha43();
+        zadacha44();
     }
 
     public static void zadacha24() {
@@ -46,20 +48,49 @@ public class JR {
             while (c > 0) {
                 if (a == 10 || a == 1) {
                     System.out.print("Q");
+                } else {
+                    if (a < 10 && a > 1 && (c == b || c == 1)) {
+                        System.out.print("Q");
+                    } else {
+                        System.out.print(" ");
+                    }
                 }
-                else{
-                if (a < 10 && a > 1 && (c == b || c == 1)) {
-                    System.out.print("Q");
-                }
-                else {
-                    System.out.print(" ");
-                }
-            }
                 c--;
             }
             System.out.println();
             a--;
         }
+    }
+
+    public static void zadacha43() {
+        Scanner sc = new Scanner(System.in);
+        int min = sc.nextInt();
+        int mtwo = sc.nextInt();
+        if (min > mtwo) {
+            int temp = min;
+            min = mtwo;
+            mtwo = temp;
+        }
+        while (sc.hasNextInt()) {
+            int value = sc.nextInt();
+            if (value < min) {
+                mtwo = min;
+                min = value;
+            }
+        }
+
+        System.out.println("MIN Vtoroe => " + mtwo + "      " + min);
+        sc.close();
+    }
+
+    public static void zadacha44() {
+        for (int i = 0; i < 10; i++)
+{
+   int starCount = 10 - i;
+   for (int j = 0; j < starCount; j++)
+      System.out.print("*");
+   System.out.println();
+}
     }
 
 }
