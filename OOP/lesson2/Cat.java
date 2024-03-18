78cbef16497baccaf619c4faec39fe22faf34bfc
Chiefpro <chiefpro@yandex.ru>
2024-03-18 19:48:30 +0300

    OOP lesson 2.0

---
+++ OOP/lesson2/Cat.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class Cat {
+    private String name;
+    private int appetite;
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+    public void eat() { }
+    
+}
