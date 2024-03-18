c08f866707dfff3681845172659d4a5372453cac
Chiefpro <chiefpro@yandex.ru>
2024-03-18 21:13:39 +0300

    OOP lesson 2.3

--- OOP/lesson2/Cat.java
+++ OOP/lesson2/Cat.java
@@ -3,10 +3,29 @@ package lesson2;
 public class Cat {
     private String name;
     private int appetite;
+    private boolean isFull;
+//    -------------------------------------------
     public Cat(String name, int appetite) {
         this.name = name;
         this.appetite = appetite;
+        this.isFull = false;
     }
-    public void eat() { }
-    
+    public void eat(lesson2.Plate plate) {
+        if (plate.getFood() >= appetite){
+            plate.setFood((plate.getFood()) - appetite);
+            isFull = true;
+        }
+    }
+    public boolean isFull() {
+        return isFull;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
 }
